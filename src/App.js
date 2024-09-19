76ba1679cc6659f4cf4f3441746ebe102fce77aa
Lucas <velvettouch1004@gmail.com>
2024-09-19 09:38:23 -0500

    hosting test

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+// import { gql, GraphQLClient } from "graphql-request";
+
+// const graphQLClient = new GraphQLClient("http://localhost:5000", {
+//   headers: {
+//     authorization: "token",
+//   },
+// });
+// const query = gql`
+//   {
+//     getTodo {
+//       id
+//       title
+//       note
+//     }
+//   }
+// `;
+// const results = await graphQLClient.request(query);
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>sending request to graphql server</h1>
+      {results.getTodo.map((value) => (
+        <div key={value.id}>
+          <h2>{value.title}</h2>
+          <p>{value.note}</p>
+        </div>
+      ))} */}
+      react
+    </div>
+  );
+}
+
+export default App;
